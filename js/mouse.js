4eab744307d9588926b206ba6df4a31d2208f301
haesol <haesol.k01@gmail.com>
2022-04-15 18:03:28 +0900

    third commit -READ ME 파일 추가 -안쓰는 파일 삭제

--- js/mouse.js
+++
@@ -1,7 +0,0 @@
-const mouse = document.querySelector("#mouse");
-
-function mousefunc(event){
-  let X = event.clientX;
-  let Y = event.clientX;
-}
-document.addEventListener("mousemove", mousefunc)
---
+++ readme.md
@@ -0,0 +1,11 @@
+# 2022-1st-portfolio
+
+***
+
+**2022년 포트폴리오입니다.**
+
+[링크(바로가기)](https://sooool-kim.github.io/2022-1st-portfolio/)<br>
+<br>
+↓ 이미지로 미리보기 ↓
+
+![이미지로 미리보기](img/readme.png)
